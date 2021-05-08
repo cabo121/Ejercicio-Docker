6dde62c25c0cbdf5356afe2f2a24b04e5cc935a1
cabo121 <armandocabo@hotmail.com>
2021-05-07 19:02:20 -0500

    2

--- curri/models.py
+++ curri/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-
+class post (models.Model):
+	name = models.CharField(default = "",null=True,max_length = 200)
+	descripcion = models.TextField(default = "",null=True,max_length = 200)
+	fecha = models.CharField(default = "",null=True,max_length = 200)
+	autor = models.CharField(default = "",null=True,max_length = 200)
+	
+	def __str__ (self):
+		return self.name
--- curri/views.py
+++ curri/views.py
@@ -4,5 +4,48 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required, permission_required
+from django.models import post
 
+#---------------------------------------------------------------
+
+class HomePageView(ListView):
+	model = post
+	template_name = 'home.html'
+	context_object_name = 'docs_list'
+
+class CurriPageView(ListView):
+	model = post
+	template_name = 'curriculum.html'
+	context_object_name = 'docs_list'
+
+class InfoPageView(ListView):
+	model = post
+	template_name = 'info.html'
+	context_object_name = 'docs_list'
+
+#---------------------------------------------------------------
+
+class RegistrarPageView (CreateView):
+	model = User
+	template_name = 'registrar.html'
+	form_class =  UserCreationForm
+	success_url = reverse_lazy('registro_success')
+
+class RegistroPageView(ListView):
+	model = coronavirus
+	template_name = 'registro_success.html'
+
+class ResetPageView (CreateView):
+	model = User
+	form_class =  UserCreationForm
+	template_name = 'registration/reset.html'
+	success_url = reverse_lazy('home')
+
+def registro_usuario (request):
+	data = {
+		'form': CustomUser()
+	}
+	return render(request, 'registrar.html', data)	
+
+#---------------------------------------------------------------
 
